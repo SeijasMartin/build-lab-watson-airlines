7787c75954edbab5c1b8bc1acecc415887ac73bb
SeijasMartin <martinseijas8@gmail.com>
2024-04-16 15:28:54 -0300

    vuelos con airports
    pagina para mostrar los vuelos segun aeropuerto de origen y destino

--- source/front-end/frontendFunctions.js
+++ source/front-end/frontendFunctions.js
@@ -24,3 +24,18 @@ async function getAirportsFromBackend() {
         throw error;
     }
 }
+
+async function getFlightsFromBackendORDE(originAirport, destinationAirport) {
+    try {
+        // Construir la URL del backend
+        const backendUrl = `https://backend-88.1fns2dopbijw.us-east.codeengine.appdomain.cloud/flightsORDE?origin_airport=${originAirport}&destination_airport=${destinationAirport}`;
+
+        // Obtener los vuelos desde el backend
+        const response = await fetch(backendUrl);
+        const data = await response.json();
+        return data.result;
+    } catch (error) {
+        console.error('Error al obtener los vuelos:', error);
+        throw error;
+    }
+}
